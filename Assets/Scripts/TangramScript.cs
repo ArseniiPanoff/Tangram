ed4f560cfdcba46a3e22b40960555716e63c7e83
Arsenii <39829327+arsenii10000@users.noreply.github.com>
2021-08-05 19:11:06 +0300

    Speed update

--- Assets/Scripts/TangramScript.cs
+++ Assets/Scripts/TangramScript.cs
@@ -6,6 +6,7 @@ public class TangramScript : MonoBehaviour
 {
     public bool Tap = false;
     private float t = 0;
+    public float speed = 35f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +16,11 @@ public class TangramScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        speed += 0.05f;
         t += Time.deltaTime;
         if (Tap)
         {
-            gameObject.GetComponent<Rigidbody2D>().MoveRotation(-25f*t);
+            gameObject.GetComponent<Rigidbody2D>().MoveRotation(-speed*t);
             //gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.05f, 0), ForceMode2D.Impulse);
         }
         
